ffb307020ea74d0ebdeac9c5a8cdfc9b92e5d564
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-03-30 20:52:04 -0700

    must use yarn start in order to work the react app

--- igradus/src/App.js
+++ igradus/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <h1>iGradus</h1>
+    )
+  }
 }
 
 export default App;
