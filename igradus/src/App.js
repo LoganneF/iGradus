24a77e56fc33e614e5b5c76242b88c0fd6e4ab44
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-03-31 16:24:58 -0700

    working on classroom

--- igradus/src/App.js
+++ igradus/src/App.js
@@ -4,9 +4,27 @@ import './App.css';
 class StudentCard extends React.Component {
   render() {
     return (
-      <div className="studentCard">
-        <img src="favicon.ico" alt="Sudent Image" className="studentCardImage" height="35" width="35"/>
-        <h5>Phillip Morris</h5>
+      <div>
+        <div className="studentCard">
+          <img src="favicon.ico" alt="Student Image" className="studentCardImage" height="35" width="35"/>
+          <h5>Chris South</h5>
+        </div>
+        <div className="studentCard">
+          <img src="favicon.ico" alt="Student Image" className="studentCardImage" height="35" width="35"/>
+          <h5>Loganne Featherston</h5>
+        </div>
+        <div className="studentCard">
+          <img src="favicon.ico" alt="Student Image" className="studentCardImage" height="35" width="35"/>
+          <h5>Marco Riesgo</h5>
+        </div>
+        <div className="studentCard">
+          <img src="favicon.ico" alt="Student Image" className="studentCardImage" height="35" width="35"/>
+          <h5>Chris Murphy</h5>
+        </div>
+        <div className="studentCard">
+          <img src="favicon.ico" alt="Student Image" className="studentCardImage" height="35" width="35"/>
+          <h5>Ira Herman</h5>
+        </div>
       </div>
     )
   }
@@ -20,8 +38,10 @@ class App extends React.Component {
           <img src="logo.png" alt="iGradus logo" className="logo" height="50" width="50"/>
           <h1>iGradus</h1>
         </div>
-        <div className="classroomContent">
+        <div className="classroomIntroduction">
           <h3>My Classroom</h3>
+        </div>
+        <div className="studentList">
           <StudentCard />
         </div>
       </div>
