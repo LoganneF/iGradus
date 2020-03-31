24b7dbbae1ef51b377e86daebbc3a24056ea57cd
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-03-30 22:30:17 -0700

    working on the student name card

--- igradus/src/App.js
+++ igradus/src/App.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
 import './App.css';
 
+class StudentCard extends React.Component {
+  render() {
+    return (
+      <div className="studentCard">
+        <img src="favicon.ico" alt="Sudent Image" className="studentCardImage" height="35" width="35"/>
+        <h5>Phillip Morris</h5>
+      </div>
+    )
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
-      <div className="navBar">
-        <img src="logo.png" alt="iGradus logo" className="logo" height="50" width="50"/>
-        <h1>iGradus</h1>
+      <div className="App">
+        <div className="navBar">
+          <img src="logo.png" alt="iGradus logo" className="logo" height="50" width="50"/>
+          <h1>iGradus</h1>
+        </div>
+        <div className="classroomContent">
+          <h3>My Classroom</h3>
+          <StudentCard />
+        </div>
       </div>
-      
     )
   }
 }
