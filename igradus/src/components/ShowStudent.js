cbc771aa7875ae1f75405f4b854249882fc9cbd6
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-04-02 01:24:18 -0700

    changed positioning of the close show modal buttons

--- igradus/src/components/ShowStudent.js
+++ igradus/src/components/ShowStudent.js
@@ -6,12 +6,12 @@ class ShowStudent extends React.Component {
       <>
       <div className="student-details">
         <h3>Student Information:</h3>
-        <button type="button" class="btn btn-outline-secondary btn-sm" id="close-modal-button" onClick={this.props.closeInfoModal} >Close</button>
         <h4>{this.props.student.name}</h4>
         <img src={this.props.student.imageUrl} alt="" className="student-info-image"/>
         <p><span>Grade:</span> { this.props.student.grade } </p>
         <button type="button" class="btn btn-danger" id="modify-student-button" onClick={() => this.props.deleteStudent(this.props.student._id)}>Remove Student</button>
-        <button type="button" class="btn btn-info" id="modify-student-button" onClick={() => this.props.deleteStudent(this.props.student._id)}>Edit Student</button>
+        <button type="button" class="btn btn-info" id="modify-student-button" onClick={() => this.props.deleteStudent(this.props.student._id)}>Edit Student</button> <br />
+        <button type="button" class="btn btn-outline-secondary btn-sm" id="close-button" onClick={this.props.closeInfoModal} >Close</button>
        </div>
       </>
     )
